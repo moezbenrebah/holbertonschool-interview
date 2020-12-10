9cc7c8433f5ab329bbc2be974f4fe2172fd860fb
Boul3ez85 <benrebah.moez@gmail.com>
2020-12-10 16:01:37 +0100

    unlock boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+
+    if boxes is None and not isinstance(boxes, list) and boxes == []:
+        return False
+    else:
+        for i in range(len(boxes)):
+            if not isinstance(i, list):
+                return False
+            else:
+                for j in i:
+                    if not isinstance(j, int):
+                        return False
+                    else:
+                        if boxes[i][j] == boxes[i]:
+                            return True
+                        else:
+                            return False 

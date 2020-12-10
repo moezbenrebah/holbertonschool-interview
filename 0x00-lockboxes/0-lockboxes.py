b9cd7a0699cfa996df83c481855d60bb18fc623c
Boul3ez85 <benrebah.moez@gmail.com>
2020-12-10 19:21:28 +0100

    determine if all boxes can be opened

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
+"""A method that determines if all the boxes can be opened."""
 
 
 def canUnlockAll(boxes):
+    """
+    Represent a method to determine if a sub_list has a key
+    for the next sub_list
+    """
 
-    if boxes is None and not isinstance(boxes, list) and boxes == []:
+    if not isinstance(boxes, list) or boxes is None:
         return False
-    else:
-        for i in range(len(boxes)):
-            if not isinstance(i, list):
+
+    if boxes[0] == []:
+        return False
+
+    opened_sub_list = [0]
+    for i in range(len(boxes)):
+        for j in boxes[i]:
+            if not isinstance(j, int):
                 return False
             else:
-                for j in i:
-                    if not isinstance(j, int):
-                        return False
-                    else:
-                        if boxes[i][j] == boxes[i]:
-                            return True
-                        else:
-                            return False
+                if j not in opened_sub_list and j < len(boxes) and j != i:
+                    opened_sub_list.append(j)
+    if len(opened_sub_list) == len(boxes):
+        return True
+    return False

5c774a48e832830c67cb6f3bf16e62bb05671460
Moez <57503360+Boul3ez85@users.noreply.github.com>
2020-12-22 15:40:52 +0100

    Update 0-binary_tree_node.c

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -17,9 +17,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
                 return (NULL);
 
         new->n = value;
-        new->parent = parent;
         new->left = NULL;
         new->right = NULL;
+        new->parent = parent;
 
         return (new);
 }

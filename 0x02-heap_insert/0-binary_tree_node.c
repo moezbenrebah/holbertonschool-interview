90b2eea064833816fc4d6985b10f0484f772c376
Moez <57503360+Boul3ez85@users.noreply.github.com>
2020-12-22 16:32:50 +0100

    Update 0-binary_tree_node.c

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
         binary_tree_t *new;
 
-        new = (struct binary_tree_s *) malloc(sizeof(binary_tree_s));
+        new = (struct binary_tree_s *) malloc(sizeof(binary_tree_t));
         if (!new)
                 return (NULL);
 

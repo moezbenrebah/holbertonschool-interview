827892558fc04ec1f0777206aa8fe22fd0a34e01
Moez <57503360+Boul3ez85@users.noreply.github.com>
2020-12-22 11:17:10 +0100

    fix syntax

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,11 +1,9 @@
-#ifndef _BINARYTREE_H
-#define _BINARYTREE_H
 
+#ifndef BINARY
+#define BINARY
+#include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -16,15 +14,13 @@
  */
 typedef struct binary_tree_s
 {
-        int n;
-        struct binary_tree_s *parent;
-        struct binary_tree_s *left;
-        struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 } binary_tree_t;
-
 typedef struct binary_tree_s heap_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
-
-#endif /* BINARYTREE_H */
+#endif

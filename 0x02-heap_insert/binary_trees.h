c931043ae97366edb8b100a825d7f8f9459205ed
Moez <57503360+Boul3ez85@users.noreply.github.com>
2020-12-22 11:13:05 +0100

    fix typo in guarded

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,12 +1,10 @@
 #ifndef _BINARYTREE_H
 #define _BINARYTREE_H
 
-
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -28,6 +26,5 @@ typedef struct binary_tree_s heap_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
-
 
 #endif /* BINARYTREE_H */

d89de3865a6d40a828db6078f83e1bae380d5b19
Boul3ez85 <benrebah.moez@gmail.com>
2021-07-22 21:58:20 +0100

    fix code

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -45,19 +45,6 @@ def N_queen(n):
     return False
 
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-try:
-    n = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
-
-if n < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
 N_queen(N)
 for i in board:
     print(i)

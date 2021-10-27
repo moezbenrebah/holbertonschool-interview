a2d5b62ba6205e0cebe7152ee0ac2f60cc0d65c0
Boul3ez85 <benrebah.moez@gmail.com>
2021-10-27 09:30:32 +0100

    make change

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" MakeChange module"""
+
+
+def makeChange(coins, total):
+    """
+    find the minimum number of coins that meets the total number
+    """
+    if total == 0:
+        return 0
+
+    coins.sort()
+    i = len(coins) - 1
+    arr = []
+    while i >= 0:
+        if sum(coins) < total:
+            return -1
+        if coins[i] <= total:
+            total -= coins[i]
+            arr.append(coins[i])
+        i -= 1
+
+    return len(arr)

b9e8ae8b4e2ae6321aafd689144aae5e23d1dc47
Boul3ez85 <benrebah.moez@gmail.com>
2021-10-27 12:36:14 +0100

    update code

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -6,20 +6,20 @@ def makeChange(coins, total):
     """
     find the minimum number of coins that meets the total number
     """
-    if total == 0:
+    if total <= 0:
         return 0
-    if not coins or min(coins) > total:
+        
+    if min(coins) > total:
         return -1
 
-    coins.sort()
-    i = len(coins) - 1
-    arr = []
-    while i >= 0:
-        if sum(coins) < total:
-            return -1
-        if coins[i] <= total:
-            total -= coins[i]
-            arr.append(coins[i])
-        i -= 1
-
-    return len(arr)
+    INT_MAX = 1<<32
+    totalSize = [INT_MAX] * (total + 1)
+        
+    totalSize[0] = 0
+        
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                totalSize[i] = min((totalSize[i-coin] + 1), totalSize[i])
+                    
+    return totalSize[total] if totalSize[total] != INT_MAX else -1

54ea4e05fbdd6ef6cc9917603825648550151d4f
moezbenrebah <benrebah.moez@gmail.com>
2022-09-13 10:35:07 +0100

    test

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,21 +1,47 @@
 #!/usr/bin/node
 
-const request = require('request');
-const version = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${version}/`;
+// const request = require('request');
+// const version = process.argv[2];
+// const url = `https://swapi-api.hbtn.io/api/films/${version}/`;
 
-request(url, async (error, response, body) => {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    for (const charId in characters) {
-      await new Promise((resolve, reject) => {
-        request(characters[charId], (error, response, body) => {
-          if (!error) {
-            console.log(JSON.parse(body).name);
-            resolve();
-          }
-        });
-      });
+// request(url, async (error, response, body) => {
+//   if (!error) {
+//     const characters = JSON.parse(body).characters;
+//     for (const charId in characters) {
+//       await new Promise((resolve, reject) => {
+//         request(characters[charId], (error, response, body) => {
+//           if (!error) {
+//             console.log(JSON.parse(body).name);
+//             resolve();
+//           }
+//         });
+//       });
+//     }
+//   }
+// });
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv.slice(2);
+
+const GetData = (l, index) => {
+  request.get(l[index], (error, res, body) => {
+
+    const ListSize = l.length;
+
+    if (error) throw error;
+    const ReturnValue = JSON.parse(body);
+    console.log(ReturnValue.name);
+
+    if (index < (ListSize - 1)) {
+      GetData(l, (index + 1));
     }
+  });
+};
+
+request.get(url, (error, res) => {
+  if (error) console.log(error);
+  else {
+    const ll = JSON.parse(res.body);
+    GetData(ll.characters, 0);
   }
 });

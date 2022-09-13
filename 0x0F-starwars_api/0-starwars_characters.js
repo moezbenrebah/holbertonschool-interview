709c721654f120a2bcaf8d03bef79e2ba6007987
moezbenrebah <benrebah.moez@gmail.com>
2022-09-13 10:54:56 +0100

    test5

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,27 +4,19 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv.slice(2);
 
 const GetData = (l, index) => {
-  request.get(l[index], (error, response, body) => {
-
+  request.get(l[index], (error, res, body) => {
     const ListSize = l.length;
-
-    if (error) {
-      throw error;
-    }
-
+    if (error) throw error;
     const ReturnValue = JSON.parse(body);
     console.log(ReturnValue.name);
-
-    if (index < (ListSize - 1)) {
-      GetData(l, (index + 1));
-    }
+    if (index < (ListSize - 1)) GetData(l, (index + 1));
   });
 };
 
-request.get(url, (error, response) => {
+request.get(url, (error, res) => {
   if (error) console.log(error);
   else {
     const ll = JSON.parse(res.body);
     GetData(ll.characters, 0);
-  }
+  };
 });

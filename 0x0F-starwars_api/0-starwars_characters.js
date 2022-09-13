1cf3d696255bc06eacfa034f28aab87d71e6ce25
moezbenrebah <benrebah.moez@gmail.com>
2022-09-13 11:18:23 +0100

    fetch starsAPI && grab actors names

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,26 +1,21 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv.slice(2);
+const version = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${version}/`;
 
-const getData = (list_1, index) => {
-  request.get(list_1[index], (error, response, body) => {
-    const ListSize = list_1.length;
-
-    if (error) throw error;
-    const ReturnValue = JSON.parse(body);
-    console.log(ReturnValue.name);
-
-    if (index < (ListSize - 1)) {
-      getData(list_1, (index + 1));
-    }
-  });
-};
-
-request.get(url, (error, response) => {
-  if (error) console.log(error);
-  else {
-    const list_2 = JSON.parse(response.body);
-    getData(list_2.characters, 0);
-  }
+request(url, async (error, response, body) => {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    for (const charId in characters) {
+      await new Promise((resolve, reject) => {
+        request(characters[charId], (error, response, body) => {
+          if (!error) {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
+        });
+      });
+    };
+  };
 });

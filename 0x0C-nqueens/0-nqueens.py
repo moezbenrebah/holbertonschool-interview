d53155ce38164fedc0a7137cf2ec8308359c050d
moezbenrebah <benrebah.moez@gmail.com>
2022-11-04 15:34:58 +0100

    test

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -99,48 +99,40 @@
 """
 print combinations of n-queen
 """
-import sys
-
-solution = []
-
-
-def solve_queens(row, n, solution):
-    """solve queens with backtracking"""
-    if (row == n):
-        print(solution)
-    else:
-        for col in range(n):
-            placement = [row, col]
-            if valid_placement(solution, placement):
-                solution.append(placement)
-                solve_queens(row + 1, n, solution)
-                solution.remove(placement)
-
-
-def valid_placement(solution, placement):
-    """cehck if position is valid using symmetry property"""
-    for queen in solution:
-        if queen[1] == placement[1]:
-            return False
-        if (queen[0] + queen[1]) == (placement[0] + placement[1]):
-            return False
-        if (queen[0] - queen[1]) == (placement[0] - placement[1]):
-            return False
-    return True
-
+from sys import argv, exit
 
 if __name__ == "__main__":
-    """main"""
-    if len(sys.argv) != 2:
+    if len(argv) != 2:
         print('Usage: nqueens N')
-        sys.exit(1)
+        exit(1)
     try:
-        n = int(sys.argv[1])
-    except Exception:
+        n = int(argv[1])
+    except BaseException:
         print('N must be a number')
-        sys.exit(1)
+        exit(1)
     if n < 4:
         print('N must be at least 4')
-        sys.exit(1)
+        exit(1)
 
-    solve_queens(0, n, solution)
+    solution = []
+
+    def solve_queens(row, n, solution):
+        if (row == n):
+            print(solution)
+        else:
+            for col in range(n):
+                placement = [row, col]
+                if valid_placement(solution, placement):
+                    solution.append(placement)
+                    solve_queens(row + 1, n, solution)
+                    solution.remove(placement)
+
+    def valid_placement(solution, placement):
+        for queen in solution:
+            if queen[1] == placement[1]:
+                return False
+            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
+                return False
+            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
+                return False
+        return True

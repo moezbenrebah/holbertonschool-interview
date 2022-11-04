ea7f436cf75de595ceb04e151dced7210a3f092e
moezbenrebah <benrebah.moez@gmail.com>
2022-11-04 15:38:38 +0100

    test

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,139 +1,96 @@
-# #!/usr/bin/python3
-
-# import sys
-
-
-# def printBoard(chessboard):
-#     """
-#     Prints the board
-#     Args:
-#         board: N*N dimensions chessboard
-
-#     Return: nothing
-#     """
-#     result = []
-#     for i in range(N):
-#         for j in range(N):
-#             if chessboard[i][j] == 1:
-#                 result.append([i, j])
-#     print(result)
-
-
-# def check_position(chessboard, row, col):
-#     """
-#     Check_position: checks if a queen is attacked by another queen
-
-#     Args
-#     chessboard (list of lists): chessboard with N*N dimensions
-#     row (int): row position
-#     col int): column position
-#     Return:
-#         boolean
-#     """
-#     for i in range(col):
-#         if chessboard[row][i]:
-#             return False
-#     i, j = row, col
-
-#     while i >= 0 and j >= 0:
-#         if chessboard[i][j]:
-#             return False
-#         i -= 1
-#         j -= 1
-
-#     i, j = row, col
-
-#     while j >= 0 and i < N:
-#         if chessboard[i][j]:
-#             return False
-#         i = i + 1
-#         j = j - 1
-
-#     return True
-
-
-# def backtrack(chessboard, col):
-#     """
-#     backtrack: a function to execute backtrack to set queens positions
-
-#     Args:
-#         board (list of lists): chessboard with N*N dimensions
-#         col (int): column position
-#     Return:
-#         boolean
-#     """
-#     if col == N:
-#         printBoard(chessboard)
-#         return True
-#     res = False
-#     for i in range(N):
-#         if check_position(chessboard, i, col):
-#             chessboard[i][col] = 1
-#             res = backtrack(chessboard, col + 1) or res
-#             chessboard[i][col] = 0
-
-#     return res
-
-
-# if __name__ == '__main__':
-
-#     if len(sys.argv) != 2:
-#         print("Usage: nqueens N")
-#         sys.exit(1)
-
-#     if not sys.argv[1].isdigit():
-#         print("N must be a number")
-#         sys.exit(1)
-
-#     N = int(sys.argv[1])
-
-#     if N < 4:
-#         print("N must be at least 4")
-#         sys.exit(1)
-
-#     chessboard = [[0 for j in range(N)] for i in range(N)]
-#     possilibities = backtrack(chessboard, 0)
-
-
 #!/usr/bin/python3
-"""
-print combinations of n-queen
-"""
-from sys import argv, exit
 
-if __name__ == "__main__":
-    if len(argv) != 2:
-        print('Usage: nqueens N')
-        exit(1)
-    try:
-        n = int(argv[1])
-    except BaseException:
-        print('N must be a number')
-        exit(1)
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
+import sys
 
-    solution = []
 
-    def solve_queens(row, n, solution):
-        if (row == n):
-            print(solution)
-        else:
-            for col in range(n):
-                placement = [row, col]
-                if valid_placement(solution, placement):
-                    solution.append(placement)
-                    solve_queens(row + 1, n, solution)
-                    solution.remove(placement)
+def printBoard(chessboard):
+    """
+    Prints the board
+    Args:
+        board: N*N dimensions chessboard
 
-    def valid_placement(solution, placement):
-        for queen in solution:
-            if queen[1] == placement[1]:
-                return False
-            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
-                return False
-            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
-                return False
+    Return: nothing
+    """
+    result = []
+    for i in range(N):
+        for j in range(N):
+            if chessboard[i][j] == 1:
+                result.append([i, j])
+    print(result)
+
+
+def check_position(chessboard, row, col):
+    """
+    Check_position: checks if a queen is attacked by another queen
+
+    Args
+    chessboard (list of lists): chessboard with N*N dimensions
+    row (int): row position
+    col int): column position
+    Return:
+        boolean
+    """
+    for i in range(col):
+        if chessboard[row][i]:
+            return False
+    i, j = row, col
+
+    while i >= 0 and j >= 0:
+        if chessboard[i][j]:
+            return False
+        i -= 1
+        j -= 1
+
+    i, j = row, col
+
+    while j >= 0 and i < N:
+        if chessboard[i][j]:
+            return False
+        i = i + 1
+        j = j - 1
+
+    return True
+
+
+def backtrack(chessboard, col):
+    """
+    backtrack: a function to execute backtrack to set queens positions
+
+    Args:
+        board (list of lists): chessboard with N*N dimensions
+        col (int): column position
+    Return:
+        boolean
+    """
+    if col == N:
+        printBoard(chessboard)
         return True
-    solve_queens(0, n, solution)
+    res = False
+    for i in range(N):
+        if check_position(chessboard, i, col):
+            chessboard[i][col] = 1
+            res = backtrack(chessboard, col + 1) or res
+            chessboard[i][col] = 0
+
+    return res
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(sys.argv[1])
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    chessboard = [[0 for j in range(N)] for i in range(N)]
+    possilibities = backtrack(chessboard, 0)
+
